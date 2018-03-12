6084fd1d0a4f605d2f08d21f4470d2a58c749549
Thomas Cataldo <thomas.cataldo@blue-mind.net>
2018-03-12 17:30:49 +0100

    Provider a faster way (allocation-free) to iterate vertx multimaps

--- vertx-core/src/main/java/org/vertx/java/core/MultiMap.java
+++ vertx-core/src/main/java/org/vertx/java/core/MultiMap.java
@@ -19,156 +19,175 @@ package org.vertx.java.core;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Consumer;
 
 /**
  * @author <a href="mailto:nmaurer@redhat.com">Norman Maurer</a>
  */
 public interface MultiMap extends Iterable<Map.Entry<String, String>> {
 
-  String get(CharSequence name);
+	String get(CharSequence name);
 
-  /**
-   * Returns the value of with the specified name.  If there are
-   * more than one values for the specified name, the first value is returned.
-   *
-   * @param name The name of the header to search
-   * @return The first header value or {@code null} if there is no such entry
-   */
-  String get(String name);
+	/**
+	 * Returns the value of with the specified name. If there are more than one
+	 * values for the specified name, the first value is returned.
+	 *
+	 * @param name
+	 *            The name of the header to search
+	 * @return The first header value or {@code null} if there is no such entry
+	 */
+	String get(String name);
 
-  /**
-   * Returns the values with the specified name
-   *
-   * @param name The name to search
-   * @return A immutable {@link List} of values which will be empty if no values
-   *         are found
-   */
-  List<String> getAll(String name);
-  List<String> getAll(CharSequence name);
+	/**
+	 * Returns the values with the specified name
+	 *
+	 * @param name
+	 *            The name to search
+	 * @return A immutable {@link List} of values which will be empty if no values
+	 *         are found
+	 */
+	List<String> getAll(String name);
 
+	List<String> getAll(CharSequence name);
 
-  /**
-   * Returns all entries it contains.
-   *
-   * @return A immutable {@link List} of the name-value entries, which will be
-   *         empty if no pairs are found
-   */
-  List<Map.Entry<String, String>> entries();
+	/**
+	 * Returns all entries it contains.
+	 *
+	 * @return A immutable {@link List} of the name-value entries, which will be
+	 *         empty if no pairs are found
+	 */
+	List<Map.Entry<String, String>> entries();
 
-  /**
-   * Checks to see if there is a value with the specified name
-   *
-   * @param name The name to search for
-   * @return True if at least one entry is found
-   */
-  boolean contains(String name);
-  boolean contains(CharSequence name);
+	void forEachEntries(Consumer<Map.Entry<String, String>> cons);
 
-  /**
-   * Return true if empty
-   */
-  boolean isEmpty();
+	/**
+	 * Checks to see if there is a value with the specified name
+	 *
+	 * @param name
+	 *            The name to search for
+	 * @return True if at least one entry is found
+	 */
+	boolean contains(String name);
 
-  /**
-   * Gets a immutable {@link Set} of all names
-   *
-   * @return A {@link Set} of all names
-   */
-  Set<String> names();
+	boolean contains(CharSequence name);
 
-  /**
-   * Adds a new value with the specified name and value.
-   *
-   *
-   * @param name The name
-   * @param value The value being added
-   *
-   * @return {@code this}
-   */
-  MultiMap add(String name, String value);
-  MultiMap add(CharSequence name, CharSequence value);
+	/**
+	 * Return true if empty
+	 */
+	boolean isEmpty();
 
-  /**
-   * Adds a new values under the specified name
-   *
-   *
-   * @param name The name being set
-   * @param values The values
-   * @return {@code this}
-   */
-  MultiMap add(String name, Iterable<String> values);
-  MultiMap add(CharSequence name, Iterable<CharSequence> values);
+	/**
+	 * Gets a immutable {@link Set} of all names
+	 *
+	 * @return A {@link Set} of all names
+	 */
+	Set<String> names();
 
-  /**
-   * Adds all the entries from another MultiMap to this one
-   *
-   * @return {@code this}
-   */
-  MultiMap add(MultiMap headers);
+	/**
+	 * Adds a new value with the specified name and value.
+	 *
+	 *
+	 * @param name
+	 *            The name
+	 * @param value
+	 *            The value being added
+	 *
+	 * @return {@code this}
+	 */
+	MultiMap add(String name, String value);
 
-  /**
-   * Adds all the entries from a Map to this
-   *
-   * @return {@code this}
-   */
-  MultiMap add(Map<String, String> headers);
+	MultiMap add(CharSequence name, CharSequence value);
 
-  /**
-   * Sets a value under the specified name.
-   *
-   * If there is an existing header with the same name, it is removed.
-   *
-   * @param name The name
-   * @param value The value
-   * @return {@code this}
-   */
-  MultiMap set(String name, String value);
-  MultiMap set(CharSequence name, CharSequence value);
+	/**
+	 * Adds a new values under the specified name
+	 *
+	 *
+	 * @param name
+	 *            The name being set
+	 * @param values
+	 *            The values
+	 * @return {@code this}
+	 */
+	MultiMap add(String name, Iterable<String> values);
 
-  /**
-   * Sets values for the specified name.
-   *
-   * @param name The name of the headers being set
-   * @param values The values of the headers being set
-   * @return {@code this}
-   */
-  MultiMap set(String name, Iterable<String> values);
-  MultiMap set(CharSequence name, Iterable<CharSequence> values);
+	MultiMap add(CharSequence name, Iterable<CharSequence> values);
 
+	/**
+	 * Adds all the entries from another MultiMap to this one
+	 *
+	 * @return {@code this}
+	 */
+	MultiMap add(MultiMap headers);
 
-  /**
-   * Cleans this instance.
-   *
-   * @return {@code this}
-   */
-  MultiMap set(MultiMap headers);
+	/**
+	 * Adds all the entries from a Map to this
+	 *
+	 * @return {@code this}
+	 */
+	MultiMap add(Map<String, String> headers);
 
-  /**
-   * Cleans and set all values of the given instance
-   *
-   * @return {@code this}
-   */
-  MultiMap set(Map<String, String> headers);
+	/**
+	 * Sets a value under the specified name.
+	 *
+	 * If there is an existing header with the same name, it is removed.
+	 *
+	 * @param name
+	 *            The name
+	 * @param value
+	 *            The value
+	 * @return {@code this}
+	 */
+	MultiMap set(String name, String value);
 
- /**
-  * Removes the value with the given name
-  *
-  * @param name The name  of the value to remove
-  * @return {@code this}
-  */
-  MultiMap remove(String name);
-  MultiMap remove(CharSequence name);
+	MultiMap set(CharSequence name, CharSequence value);
 
+	/**
+	 * Sets values for the specified name.
+	 *
+	 * @param name
+	 *            The name of the headers being set
+	 * @param values
+	 *            The values of the headers being set
+	 * @return {@code this}
+	 */
+	MultiMap set(String name, Iterable<String> values);
 
-  /**
-   * Removes all
-   *
-   * @return {@code this}
-   */
-  MultiMap clear();
+	MultiMap set(CharSequence name, Iterable<CharSequence> values);
 
-  /**
-   * Return the number of names.
-   */
-  int size();
+	/**
+	 * Cleans this instance.
+	 *
+	 * @return {@code this}
+	 */
+	MultiMap set(MultiMap headers);
+
+	/**
+	 * Cleans and set all values of the given instance
+	 *
+	 * @return {@code this}
+	 */
+	MultiMap set(Map<String, String> headers);
+
+	/**
+	 * Removes the value with the given name
+	 *
+	 * @param name
+	 *            The name of the value to remove
+	 * @return {@code this}
+	 */
+	MultiMap remove(String name);
+
+	MultiMap remove(CharSequence name);
+
+	/**
+	 * Removes all
+	 *
+	 * @return {@code this}
+	 */
+	MultiMap clear();
+
+	/**
+	 * Return the number of names.
+	 */
+	int size();
 }
