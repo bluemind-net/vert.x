8e44a57e58c64d1e29f03b4b3be0d121471f5ac8
Thomas Cataldo <thomas.cataldo@blue-mind.net>
2018-03-16 15:58:48 +0100

    re-integrate our change into vertx

--- vertx-core/src/main/java/org/vertx/java/core/impl/VertxExecutorFactory.java
+++ vertx-core/src/main/java/org/vertx/java/core/impl/VertxExecutorFactory.java
@@ -15,20 +15,20 @@
  */
 package org.vertx.java.core.impl;
 
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import org.vertx.java.core.impl.management.ManagementRegistry;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.vertx.java.core.impl.management.ManagementRegistry;
+
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
 
 /**
  * Util factory for creating vert.x thread pools
  *
- * The pools shouldn't be too configurable by the user otherwise they
- * can get into problems. Vert.x requires quite specific behaviour from each pool
- * and things can easily break if they are configured incorrectly.
+ * The pools shouldn't be too configurable by the user otherwise they can get
+ * into problems. Vert.x requires quite specific behaviour from each pool and
+ * things can easily break if they are configured incorrectly.
  * 
  * @author swilliams
  * @author <a href="http://tfox.org">Tim Fox</a>
@@ -36,23 +36,25 @@ import java.util.concurrent.Executors;
  */
 public class VertxExecutorFactory {
 
-  public static final int WORKER_POOL_MAX_SIZE = 20;
+	public static final int WORKER_POOL_MAX_SIZE = 20;
 
-  // The worker pool needs to be fixed with a backing queue
-  public static ExecutorService workerPool(String poolName) {
-    int maxSize = Integer.getInteger("vertx.pool.worker.size", WORKER_POOL_MAX_SIZE);
-    ExecutorService exec = Executors.newFixedThreadPool(maxSize, new VertxThreadFactory(poolName));
-    ManagementRegistry.registerThreadPool("Worker", exec);
-    return exec;
-  }
+	// The worker pool needs to be fixed with a backing queue
+	public static ExecutorService workerPool(String poolName) {
+		int maxSize = Integer.getInteger("vertx.pool.worker.size", WORKER_POOL_MAX_SIZE);
+		ExecutorService exec = Executors.newFixedThreadPool(maxSize, new VertxThreadFactory(poolName));
+		ManagementRegistry.registerThreadPool("Worker", exec);
+		return exec;
+	}
 
-  // The acceptor pools need to be fixed with a backing queue
+	// The acceptor pools need to be fixed with a backing queue
 
-  public static EventLoopGroup eventLoopGroup(String poolName) {
-    return new NioEventLoopGroup(eventLoopSize(), new VertxThreadFactory(poolName));
-  }
+	public static EventLoopGroup eventLoopGroup(String poolName) {
+		NioEventLoopGroup ret = new NioEventLoopGroup(eventLoopSize(), new VertxThreadFactory(poolName));
+		ret.setIoRatio(100);
+		return ret;
+	}
 
-  public static int eventLoopSize() {
-      return Integer.getInteger("vertx.pool.eventloop.size", 2 * Runtime.getRuntime().availableProcessors());
-  }
+	public static int eventLoopSize() {
+		return Integer.getInteger("vertx.pool.eventloop.size", 2 * Runtime.getRuntime().availableProcessors());
+	}
 }
