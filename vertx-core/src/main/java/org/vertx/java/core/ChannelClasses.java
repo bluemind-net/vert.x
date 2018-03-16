ad0a1d1229111423ca09129eba3e01677a44be89
Thomas Cataldo <thomas.cataldo@blue-mind.net>
2018-03-16 16:19:30 +0100

    Feat: switch to epoll / native if available

--- vertx-core/src/main/java/org/vertx/java/core/ChannelClasses.java
+++ vertx-core/src/main/java/org/vertx/java/core/ChannelClasses.java
@@ -22,6 +22,10 @@ import org.vertx.java.core.impl.VertxThreadFactory;
 import io.netty.channel.Channel;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.ServerChannel;
+import io.netty.channel.epoll.Epoll;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
+import io.netty.channel.epoll.EpollSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -29,15 +33,27 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 public final class ChannelClasses {
 
 	public static Class<? extends ServerChannel> serverSocket() {
-		return NioServerSocketChannel.class;
+		if (Epoll.isAvailable()) {
+			return EpollServerSocketChannel.class;
+		} else {
+			return NioServerSocketChannel.class;
+		}
 	}
 
 	public static Class<? extends Channel> clientSocket() {
-		return NioSocketChannel.class;
+		if (Epoll.isAvailable()) {
+			return EpollSocketChannel.class;
+		} else {
+			return NioSocketChannel.class;
+		}
 	}
 
 	public static EventLoopGroup createLoopGroup(int threads, String poolName) {
-		return new NioEventLoopGroup(threads, new VertxThreadFactory(poolName));
+		if (Epoll.isAvailable()) {
+			return new EpollEventLoopGroup(threads, new VertxThreadFactory(poolName));
+		} else {
+			return new NioEventLoopGroup(threads, new VertxThreadFactory(poolName));
+		}
 	}
 
 }
